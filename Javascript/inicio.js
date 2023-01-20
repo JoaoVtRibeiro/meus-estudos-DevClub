3aca95332f2aa4b768041eb6a601f3f890ae86da
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-20 14:39:46 -0300

    Iniciando Novo Módulo - Javascript

---
+++ Javascript/inicio.js
@@ -0,0 +1,17 @@
+/*     
+    Javascript
+
+    Uma das linguagens de programação, utilizada principalmente para web, mas pode ser utilizada 
+    também para mobile ou programas de computador.
+
+    Linguagem de programação se trata de uma forma de comunicação com o computador para realizar uma
+    ação através de uma sequência logica de comandos (algoritmos).
+
+*/
+
+console.log("Olá") /* Comando que mostra o valor que ele conter no console (textos, números, variaveis, etc.) */
+
+
+
+
+
