e44ddd16d544d35ca005714c51cbca426406bba7
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-21 14:18:43 -0300

    Anotações - Boolean

---
+++ Javascript/anotacoes-e-rascunhos1/tipos-de-dados/boolean.js
@@ -0,0 +1,24 @@
+/* 
+    Boolean
+
+    Variaveis que trabalham com verdadeiro e falso (true = 1 false = 0)
+
+        Sinais
+
+        = Atribuição, define o valor da variavel
+
+        == Comparação, compara se o valores são iguais ou não
+
+*/
+
+let number1 = 10
+
+let number2 = 20
+
+console.log(number1 == number2)
+
+let number3 = number1 + number2
+
+let number4 = 30
+
+console.log(number3 == number4)
