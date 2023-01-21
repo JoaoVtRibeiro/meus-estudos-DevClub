8b4278385a9a37d21aa439aec7a2f6f91b461e1c
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-21 13:42:57 -0300

    Anotações - Numbers

---
+++ Javascript/anotacoes-e-rascunhos1/tipos-de-dados/numbers.js
@@ -0,0 +1,18 @@
+/* 
+    Numbers
+
+    Como nome diz, são dados que armazenam números, permitem fazer contas e afins
+
+*/
+
+let number = 120 / 2
+
+let fractionalNumber = 1.23 /* Permite números fracionados */
+
+let string = "texto"
+
+console.log(number + fractionalNumber)
+
+console.log(10 + string)
+
+/* Obs: Caso o resultado for NaN, significa que não é um número*/
