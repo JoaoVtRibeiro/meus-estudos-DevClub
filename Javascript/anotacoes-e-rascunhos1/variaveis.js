377711ab2601acb9bfa4a79a4ad4a8d97ab1c415
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-21 12:08:49 -0300

    Anotações - Variaveis

---
+++ Javascript/anotacoes-e-rascunhos1/variaveis.js
@@ -0,0 +1,33 @@
+/* 
+    Variaveis
+
+    São como "espaços" reservados no código para guardar valores e informações
+
+        Permitido:
+
+        - Iniciar $ ou _
+        - Colocar acentos
+        - Iniciar com letras
+        - Letras maiúsculas e minúsculas
+
+        Não permitido:
+
+        - Iniciar com números 
+        - Colocar espaços
+
+        Padrões comuns:
+
+        - Camel case (nomeDaVariavel, começar com letra minuscula e usar maiusculas para separar as palavras)
+        - Snake case (nome_da_variavel, utilizar o _ para separar as palavras)
+        - Sempre em inglês
+        - nomes que fazem sentido com o valor que ela tem
+
+*/
+
+    var number1 = 1 /* Definine um nome de variavel para todo o código, não podendo ser repitido (Não recomendado) */
+    let number1 = 2 /* O nome dessa variavel pode ser reutilizado em blocos diferents */
+    const number3 = 3  /* O valor dessa variavel não pode ser alterado, pois é uma constante */
+
+    console.log(number1)
+    console.log(number2)
+    console.log(number3)
