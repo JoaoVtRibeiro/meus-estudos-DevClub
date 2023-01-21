416e33bf03ca6ef3c7dc0b82831d922b67168eb3
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-21 12:33:49 -0300

    Anotações - String

---
+++ Javascript/anotacoes-e-rascunhos1/tipos-de-dados/strings.js
@@ -0,0 +1,27 @@
+/* 
+    Strings
+
+    Dados de texto, podem ter palavras, frases e caracteres
+
+*/
+
+
+let doubleQuotationMarks = "String com aspas duplas"
+
+let singleQuotationMark = 'String com aspas simples'
+
+
+/* Templete String / Templete Literals */
+
+let dollarSignAndCurlyBrackets= "${}"
+
+let templeteString = `Templete String, permite
+pular
+linhas
+e também permite utilizar variaveis através do ${dollarSignAndCurlyBrackets}
+Além de poder fazer calculos também, ex: 40 + 20 - 3 = ${40 + 20 - 3}`
+
+console.log(templeteString)
+
+
+                                    
