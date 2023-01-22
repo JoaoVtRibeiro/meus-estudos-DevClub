385197e5b3bae42d6c06d055798db6d28dac7d9b
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-22 11:35:08 -0300

    Anotações - Math

---
+++ Javascript/anotacoes-e-rascunhos1/math.js
@@ -0,0 +1,15 @@
+/* 
+    Math
+
+    Biblioteca do javascript que permite fazer algumas contas especificas ou ter acesso a algum tipo de número
+
+*/
+
+
+    console.log(Math.PI) /* Valor de PI */
+    console.log(Math.pow(2,3)) /* Pontenciação */
+    console.log(Math.sqrt(9,2)) /* Raiz de um número (quadrada, cubica...) */
+    console.log(Math.random()) /* Número aleatório entre 0 e 1 */
+    console.log(Math.ceil(23,1)) /* Arrendonda um número fracionado para "cima" */
+    console.log(Math.floor(23,1)) /* Arrendonda um número fracionado para "baixo" */
+
