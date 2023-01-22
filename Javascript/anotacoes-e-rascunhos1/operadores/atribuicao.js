6e3a39277d67711d677def79527465f3c6a02bbb
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-22 14:48:59 -0300

    Anotações - Operadores de atribuição

---
+++ Javascript/anotacoes-e-rascunhos1/operadores/atribuicao.js
@@ -0,0 +1,38 @@
+/* 
+    Operadores de atribuição
+
+    =   -> Atribuição de um valor
+    +=  -> Adição
+    -+  -> Subtração
+    /=  -> Divisão
+    *=  -> Multiplicação
+    %=  -> Resto
+
+    Na prática, são quase como atalhos, já fazem o calculo da atribuição com a própria variavel
+
+    let variavel = variavel + 10 é igual a let variavel += 10
+
+*/
+
+    let number = 10
+
+    number += 10
+
+    console.log(number)
+
+    number -= 5
+
+    console.log(number)
+
+    number *= 10
+
+    console.log(number)
+
+    number /= 2
+
+    console.log(number)
+
+    number %= 5
+    
+    console.log(number)
+
