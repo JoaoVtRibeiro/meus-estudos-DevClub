808e79dd71471127bf9d5ac9b96af39ac4eebbc5
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-22 14:35:44 -0300

    Iniciando Operadores - Aritmeticos

---
+++ Javascript/anotacoes-e-rascunhos1/operadores/aritmeticos.js
@@ -0,0 +1,24 @@
+/* 
+    Operadores Aritmeticos
+
+    + -> Adição
+    - -> Subtração
+    * -> Multiplicação
+    / -> Divisão
+    % -> Resto (Não é possivel com números decimais)
+    ++ -> Incremento (Adiciona +1)
+    -- -> Decremento (Subtrai -1)
+    ** -> Potenciação (Math.pow)
+*/
+
+    let number = 10
+
+    ++number
+
+    console.log(number)
+
+    --number
+
+    console.log(number)
+
+    console.log(number ** 3)
