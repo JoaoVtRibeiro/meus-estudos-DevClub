fb22739b247d3ed02ba094fc0882236e1364147f
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-23 13:09:27 -0300

    Anotações - Operadores de comparação

---
+++ Javascript/anotacoes-e-rascunhos1/operadores/comparacao.js
@@ -0,0 +1,18 @@
+/* 
+    Operadores de comparação
+
+    ==      -> Igual : Compara o valor, mas não compara o tipo do valor 
+    ===     -> Totalmente igual: Compara o valor e o tipo de dado (Recomendado)
+
+    !=      -> Diferente : Compara o valor, mas não compara o tipo do valor 
+    !==     -> Totalmente diferente: Compara o valor e o tipo de dado (Recomendado)
+*/
+
+const number = "20"
+
+const number2 = 20
+
+console.log(number == number2)
+
+console.log(number === number2)
+
