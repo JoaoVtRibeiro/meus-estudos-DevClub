eb50ee835a9da1aa02c505961dc7c486f605d98f
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-01-28 16:18:45 -0300

    Anotações - Condicionais If else

---
+++ Javascript/anotacoes-e-rascunhos1/condicionais/ifselse.js
@@ -0,0 +1,18 @@
+/* 
+    Condicionais - If Else
+
+    Executam um determinado trecho de código SE (if) uma condição determinada for verdadeira e outra SE NÂO (else) for verdadeira.
+
+    if = se
+    else = se não
+
+    Obs: Não é obrigatório que um if acompanhe um else (pode ser utilizado sozinho), mas o else só pode ser utilizado acompanhado de um if
+*/
+
+let age = 18
+
+if(age >= 18){
+    console.log("A pessoa é maior de idade")
+} else {
+    console.log("A pessoa é menor de idade")
+}
