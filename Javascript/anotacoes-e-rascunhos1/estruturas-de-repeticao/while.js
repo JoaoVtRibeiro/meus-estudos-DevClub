394a4fa3a5a1f74a6fba1f71fdc5d9a8c2405cf8
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-01 04:00:33 -0300

    Anotações - Estrutura de repetição While

---
+++ Javascript/anotacoes-e-rascunhos1/estruturas-de-repeticao/while.js
@@ -0,0 +1,16 @@
+/* 
+    Estrutura de repetição - While (Enquanto)
+
+    While repete um determinado trecho de código ENQUANTO a condição determinada for verdadeira.
+
+    Obs: Recomendado utilizar o while quando é imprevisel dizer quando o evento irá terminar/acontecer, como dividir um número muito grande por exemplo
+
+*/
+
+let i = 0
+
+while(i <= 20){
+    console.log(i)
+    i+= 2
+}
+
