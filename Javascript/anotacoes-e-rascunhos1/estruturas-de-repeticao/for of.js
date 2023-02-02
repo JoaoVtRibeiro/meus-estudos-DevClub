e78bc3362d030cab8da29e87eb80cb705cbb2223
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-02 08:34:56 -0300

    Anotações - For of

---
+++ Javascript/anotacoes-e-rascunhos1/estruturas-de-repeticao/for of.js
@@ -0,0 +1,21 @@
+/* 
+    Estrutura de repetição - For of
+
+    For of repete um trecho de código a partir do número de caracteres de uma string ou de posições em uma array
+*/
+
+
+// Exemplo com string
+let string = "texto"
+
+for(value of string){
+    console.log(value)
+}
+
+
+// Exemplo com array
+let array = ["pessoa1", "pessoa2", "pessoa3"]
+
+for(value of array){
+    console.log(value)
+}
