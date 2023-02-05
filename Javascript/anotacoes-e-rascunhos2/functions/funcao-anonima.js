6c70a393ac0b7e443e3095fe7870f9fa8937223c
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-05 11:17:10 -0300

    Anotações - Função Anônima

---
+++ Javascript/anotacoes-e-rascunhos2/functions/funcao-anonima.js
@@ -0,0 +1,15 @@
+/* 
+    Função Anônima
+
+    É bem parecido com a função comum, a unica coisa que muda é que a 
+    função é utilizada como valor de uma variavel, não precisando de um nome.
+
+*/
+
+let sum = function(number1, number2){
+    return number1 + number2
+}
+
+let result = sum(5,4)
+
+console.log(result)
