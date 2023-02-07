21b7f0255f68ba5019d0a07fe15bad91d16e688c
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-07 02:52:07 -0300

    Nova alternativa de resolução com outro comando - .toLocateTimeString

--- Javascript/desafios/exercicios3-function/ex03.js
+++ Javascript/desafios/exercicios3-function/ex03.js
@@ -5,3 +5,11 @@
 let date = new Date();
 
 console.log(`Agora são exatamente ${date.getHours()} horas e ${date.getMinutes()} minutos`)
+
+/* 
+    A outro comando que é possivel saber a hora, minuto e segundo a partir do UTC
+
+    new Date().toLocateTimeString('pt-BR')
+
+    Caso utilizar o .getMonth lembre-se de colocar um "+1" já que a contagem começa do zero.
+*/
