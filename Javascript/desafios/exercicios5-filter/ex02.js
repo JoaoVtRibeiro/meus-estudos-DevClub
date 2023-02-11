4026a08fe23564e550ee69c20e87bbec9d7d5246
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-11 11:40:12 -0300

    Correção no nome da variavel marketValue

--- Javascript/desafios/exercicios5-filter/ex02.js
+++ Javascript/desafios/exercicios5-filter/ex02.js
@@ -4,14 +4,14 @@
 */
 
 const companies = [
-    {name: "Samsung", marketValoue: 50, CEO: "Kim Hyun Suk", foundedOn: 1938},
-    {name: "Microsoft", marketValoue: 415, CEO: "Satya Nadella", foundedOn: 1975},
-    {name: "Intel", marketValoue: 117, CEO: "Brian Krzanich", foundedOn: 1968},
-    {name: "Facebook", marketValoue: 383, CEO: "Mark Zuckerberg", foundedOn: 2004},
-    {name: "Spotify", marketValoue: 30, CEO: "Daniel Ek", foundedOn: 2006},
-    {name: "Apple", marketValoue: 845, CEO: "Tim Cook", foundedOn: 1976},
+    {name: "Samsung", marketValue: 50, CEO: "Kim Hyun Suk", foundedOn: 1938},
+    {name: "Microsoft", marketValue: 415, CEO: "Satya Nadella", foundedOn: 1975},
+    {name: "Intel", marketValue: 117, CEO: "Brian Krzanich", foundedOn: 1968},
+    {name: "Facebook", marketValue: 383, CEO: "Mark Zuckerberg", foundedOn: 2004},
+    {name: "Spotify", marketValue: 30, CEO: "Daniel Ek", foundedOn: 2006},
+    {name: "Apple", marketValue: 845, CEO: "Tim Cook", foundedOn: 1976},
 ]
 
-const bigNewcompanies = companies.filter(company => company.marketValoue > 200 && company.foundedOn > 1975)
+const bigNewcompanies = companies.filter(company => company.marketValue > 200 && company.foundedOn > 1975)
 
 console.log(bigNewcompanies)
