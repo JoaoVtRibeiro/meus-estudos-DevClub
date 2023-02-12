404bdb89f6a67f2c56211c166518b1aa524eb333
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-12 09:57:44 -0300

    Anotações - DOM

---
+++ Javascript/anotacoes-e-rascunhos3/DOM.js
@@ -0,0 +1,7 @@
+/* 
+    DOM (Document Object Model)
+
+    É como uma representação em árvore de um documento HTML,
+    permitindo que linguagens de programação acessem e modifiquem seu conteúdo e estrutura.
+
+*/
