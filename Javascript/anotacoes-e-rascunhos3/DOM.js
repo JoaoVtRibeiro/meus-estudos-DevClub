cc97a7533ad98fb8dcf3b75b8871f7c0dc7925ca
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-12 09:59:50 -0300

    Correção na descrição da DOM

--- Javascript/anotacoes-e-rascunhos3/DOM.js
+++ Javascript/anotacoes-e-rascunhos3/DOM.js
@@ -1,7 +1,7 @@
 /* 
     DOM (Document Object Model)
 
-    É como uma representação em árvore de um documento HTML,
-    permitindo que linguagens de programação acessem e modifiquem seu conteúdo e estrutura.
+    É uma interface de programação de aplicativos (API), uma representação em árvore de um documento HTML,
+    que permite que linguagens de programação acessem e modifiquem seu conteúdo e estrutura.
 
 */
