ff6ed809ab57398a8913bc40e26b6ca503b8eb4f
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-12 15:10:17 -0300

    Anotações - Selecionando Atributos

---
+++ Javascript/anotacoes-e-rascunhos3/selecionar-atributos/selecionar-atributos.js
@@ -0,0 +1,17 @@
+/* 
+    Selecionar atributos
+
+    Além de selecionar elementos, também é possivel acessar 
+    atributos com o "getElementsBy" e "querySelector"
+
+*/
+
+// getElementById
+const inputGet = document.getElementById("input-id")
+
+console.log(inputGet.value)
+
+// querySelector
+const inputQuery = document.querySelector("#input-id")
+
+console.log(inputQuery.value)
