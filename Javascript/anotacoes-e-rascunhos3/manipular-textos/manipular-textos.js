b313d835a2e89edd4c4b8336926c4df34282c8a7
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-13 09:09:24 -0300

    Anotações - Manipular textos (innerText, textContent e innerHTML)

---
+++ Javascript/anotacoes-e-rascunhos3/manipular-textos/manipular-textos.js
@@ -0,0 +1,23 @@
+/* 
+    Manipular textos
+
+    É possivel alterar textos com os comandos:
+
+    - .innerText
+    - .textContent
+    - .innerHTML (Com esse, também é possivel inserir tags html)
+
+*/
+
+const title1 = document.querySelector("h1")
+
+const title2 = document.querySelector("h2")
+
+const paragraph = document.querySelector("p")
+
+
+title1.innerText = "Primeiro titulo alterado"
+
+title2.textContent = "Segundo titulo alterado"
+
+paragraph.innerHTML = "<i>O texto alterado para itálico</i>"
