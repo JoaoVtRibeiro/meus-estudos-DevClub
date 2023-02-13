bf8c720af14047d451fd585fd794a072e9664f3d
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-13 09:25:10 -0300

    Anotações - Manipulando estilos (.style)

---
+++ Javascript/anotacoes-e-rascunhos3/manipular-estilos/manipular-estilos.js
@@ -0,0 +1,14 @@
+/* 
+    Manipular estilos (CSS)
+
+    É possivel alterar o estilo de um elemento utilizando a propriedade ".style" + ".comandocss"
+*/
+
+const title = document.querySelector('h1')
+
+title.style.color = "red"
+
+// Obs: para comandos com "-" utilize Camel Case, ex: background-color, backgroundColor
+const body = document.querySelector('body')
+
+body.style.backgroundColor = "cyan"
