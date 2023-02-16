5b9f30a0071276f4c5a7cd7dd1e74b829b85de2c
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-16 09:08:27 -0300

    Node - Introdução

---
+++ Node/Introdução.js
@@ -0,0 +1,38 @@
+/* 
+   Frond-end e Back-End
+
+   Frond-end é a interface do usúario, utiliza tecnologias como Html, Css e javascript
+   onde ele pode visualizar e interagir, também é onde ocorre o "request"
+
+   Back-end diferente do front, não fornece interação direta com usuario,
+   através do "request" ele pode acessar informações no banco de dados
+   de um servidor e entregar uma "response"
+
+    Node
+
+    Uma plataforma de desenvolvimento javascript,
+    ele permite que o sistema operacional entenda a linguagem (javascript)
+    além disso, é possivel criar com node um servidor web, 
+    lidar com solicitações HTTP, acessar bancos de dados, processar arquivos, etc.
+
+    HTTP
+
+    É um protocolo que computadores utilizam para acessar páginas web,
+    ele especifica como será a comunição entre o navegador(request) e o servidor(response).
+    Ex: https://www.google.com/
+
+    Obs: HTTPS é basicamente o protocolo http, mas com uma camada de proteção de transmissão dos dados
+
+    Request
+
+    Uma requisição realizada pelo usuario através do front-end,
+    esse pedido contém informações que descrevem qual ação o back-end 
+    deve tomar para se comunicar com o servidor
+
+    Response
+
+    Após o envio de um request, o back-end(servidor) interpreta o pedido,
+    realiza uma determinada ação e envia uma resposta ao usuario.
+
+*/
+
