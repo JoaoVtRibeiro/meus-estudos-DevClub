3102360b801c147d7275b4113c9475fb81ceb275
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-02-23 09:27:49 -0300

    Anotações - API, Rest e Restful

---
+++ Javascript Avançado/1-API-Rest-Restful.js
@@ -0,0 +1,32 @@
+/* 
+    API- Application Programming Interface / Interface de Programação de Aplicativos
+
+    API é um software que permite que outros softwares de diversos
+    tipos de dispositivos o acessem através de um código padronizado.
+    Ela é um conjunto ferramentas, protocolos e bibliotecas
+    e com isso pode ser usada para acessar dados, funcionalidades e recursos de um software 
+    ou serviço, sem que o usuário precise entender detalhes sobre sua implementação interna.
+
+    Em resumo, a API permite a  integração de diferentes sistemas e 
+    aplicativos, promovendo a interoperabilidade e a troca de informações entre eles.
+
+
+    API Rest
+
+    Consiste em princípios e regras que permitem a criação de um projeto
+    com interfaces bem definidas. Assim, varias aplicações podem se comunicar.
+
+    Algumas dessas regras são:
+
+    - cliente-servidor: as aplicações existentes no servidor e no cliente debem ser separadas
+
+    - sem estado: as requisições são feitas de forma independente, ou seja, cada uma executa apenas uma determinada ação
+
+    - As respostas são auto-descritivas, incluindo informação suficiente para que o cliente saiba como utiliza-las
+
+    API Restful
+
+    Termo utilizado para definir uma API que segue a arquitetura Rest
+
+
+*/
