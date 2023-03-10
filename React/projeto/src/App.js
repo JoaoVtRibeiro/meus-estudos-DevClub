ea8466367409055367c719461fdaedda7120dc50
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-03-10 10:46:06 -0300

    Anotações - CSS no React

--- React/projeto/src/App.js
+++ React/projeto/src/App.js
@@ -1,11 +1,15 @@
 import React from "react"
 
+import { Container, H1 } from "./styles"; // Importação dos componentes CSS
+
 // JSX (Sintaxe que permite html e javascript no mesmo código)
 const App = () => {
     const hello = "Hello React"
 
     return (
-        <div>{hello}</div>
+        <Container>
+            <H1>{hello}</H1>
+        </Container>
     )
 }
 
---
+++ React/projeto/src/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+// export const nome = styled.tag = componente
+
+export const Container = styled.div`
+    background-color: red;
+`
+
+export const H1 = styled.h1`
+    color: blue;
+    font-size: 20px;
+`
