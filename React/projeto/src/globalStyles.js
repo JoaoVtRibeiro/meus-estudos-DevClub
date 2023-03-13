70b8b2be2fdcadcb13e6c875bf891115160bbe0a
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-03-13 13:02:00 -0300

    Anotações - Estilos Globais, Importação e Exportação

---
+++ React/projeto/src/globalStyles.js
@@ -0,0 +1,16 @@
+/* 
+    Importação do recurso "createGlobalStyle" da biblioteca "styled-components"
+
+    Esse recurso permite criar estilos globais para todos os elementos
+*/
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle` // Exportando os estilos
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}`;
+
+
--- React/projeto/src/index.js
+++ React/projeto/src/index.js
@@ -1,6 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from "react"; // Importando a biblioteca React
+import ReactDOM from "react-dom"; // Modulo/Dependencia react, permite manipular a DOM
+import App from "./App"; // Aplicação
+import GlobalStyles from './globalStyles' // Estilos globais
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( /* Método  que renderiza os componentes react na página (atualizando a árvore da DOM) 
+Ele recebe dois argumentos:
+*/
+
+// 1. Componentes React (Importados no inicio do código)
+<> {/* Fragment, usado para cumprir a regra do react (Sempre ter um "elemento pai") */}
+    <App /> <GlobalStyles />
+</>,
+
+// 2. Elemento DOM onde os componentes devem ser renderizados
+document.getElementById("root"));
+
+
+/* 
+    Lembrete sobre a exportação, quando:
+
+    export com default, import sem {}
+
+    export sem default, import com {}
+*/
 
