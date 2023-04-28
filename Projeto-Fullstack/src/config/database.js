0e564723ec0a77e9888e896b82e563aee86d5c86
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-04-28 13:08:25 -0300

    Projeto Fullstack - Descrições dos arquivos referentes ao Sequelize

--- Projeto-Fullstack/src/config/database.js
+++ Projeto-Fullstack/src/config/database.js
@@ -1,12 +1,22 @@
 module.exports = {
-    dialect: 'postgres',
+    dialect: 'postgres', // Banco de dados que será utilizado
     host: 'localhost',
     username: 'postgres',
     password: 'postgres',
     database: 'codeburger',
-    define: {
-        timespamps: true,
-        underscored: true,
-        underscoredAll: true,
+    define: { // Definições 
+        timespamps: true, // cria dois campos adicionais, "created_at" (data de criação) e "updated_at" (data de atualização)
+        underscored: true, 
+        underscoredAll: true, 
     },
 }
+
+/*   underscored e underscoredAll: 
+     - deixa a escrita dos dados em "caixa baixa" e sepadaros por "_"
+
+     - exemplo:
+
+        - sem underscored: UsersProducts
+
+        - com underscored: users_products
+*/
