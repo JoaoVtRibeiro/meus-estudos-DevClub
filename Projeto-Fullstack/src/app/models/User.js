c3371793c2c5b73e40aeb400e28ecf75f3d42883
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-05-01 12:25:47 -0300

    Projeto Fullstack - Criação do Model do Usuário

---
+++ Projeto-Fullstack/src/app/models/User.js
@@ -0,0 +1,19 @@
+import Sequelize, { Model } from 'sequelize'
+
+class User extends Model {
+    static init(sequelize){ // static faz com que o método posso ser usar sem a necessidade de instanciar a classe
+        super.init( // super permite utilizar os métodos da classe que foi extendida
+            {
+                name: Sequelize.STRING,
+                email: Sequelize.STRING,
+                password_hash: Sequelize.STRING,
+                admin: Sequelize.BOOLEAN,
+            },
+            {
+                sequelize,
+            }
+        )
+    }
+}
+
+export default User
