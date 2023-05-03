52eebffd74403a187776f7f7c35e33813bdda77e
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-05-03 18:54:28 -0300

    Projeto Fullstack - Controller do cadastro de usuário

--- Projeto-Fullstack/src/routes.js
+++ Projeto-Fullstack/src/routes.js
@@ -1,19 +1,8 @@
 import { Router } from 'express'
 
-import { v4 } from 'uuid'
-import User from './app/models/User'
-
+import UserController from './app/controllers/UserController'
 const routes = new Router()
 
-routes.get('/', async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: 'Carinha2',
-    email: 'carinha2@gmail.com',
-    password_hash: '123'
-  })
-
-  return response.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes
