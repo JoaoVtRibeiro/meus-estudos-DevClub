f08b15d814012caf49e322024c326743ecef13ee
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-05-04 15:39:14 -0300

    Projeto Fullstack - Biblioteca Yup, validação dos dados

--- Projeto-Fullstack/src/app/controllers/UserController.js
+++ Projeto-Fullstack/src/app/controllers/UserController.js
@@ -9,11 +9,34 @@
 */
 
 import { v4 } from 'uuid'
+import * as Yup from 'yup'
+
 import User from '../models/User'
 
 class UserController {
     async store(request, response) {
-        const {name, email, password_hash, admin } = request.body
+        const { name, email, password_hash, admin } = request.body
+
+        // Validação dos dados
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().required().min(6),
+            admin: Yup.boolean(),
+        })
+
+        /*  if (!(await schema.isValid(request.body))) {
+            return response.status(400).json({ error: 'Make sure your data is correct' })
+        } */
+
+        try {
+            await schema.validateSync(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
+        }
+
+        // Criação do Usuário
 
         const user = await User.create({
             id: v4(),
@@ -23,7 +46,7 @@ class UserController {
             admin,
         })
 
-        return response.status(201).json({id: user.id, name, email, admin})
+        return response.status(201).json({ id: user.id, name, email, admin })
     }
 }
 
