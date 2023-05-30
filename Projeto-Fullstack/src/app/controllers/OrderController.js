09bf19e1266d78703d1ba3f924774ab2feb27b04
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-05-30 13:18:03 -0300

    Projeto Fullstack - Finalização do controller de pedidos

--- Projeto-Fullstack/src/app/controllers/OrderController.js
+++ Projeto-Fullstack/src/app/controllers/OrderController.js
@@ -1,5 +1,8 @@
 import * as Yup from 'yup'
 
+import Product from '../models/Product'
+import Category from '../models/Category'
+
 class OrderController {
     async store(request, response) {
         const schema = Yup.object().shape({
@@ -17,17 +20,45 @@ class OrderController {
             return response.status(400).json({ error: err.errors })
         }
 
-
         const productsId = request.body.products.map(product => product.id)
+
+        const updatedProducts = await Product.findAll({
+            where: {
+                id: productsId
+            },
+            include: {
+                model: Category,
+                as: 'category',
+                attributes: ['name']
+            }
+        })
+
+        const editedProduct = updatedProducts.map((upProduct) => {
+            const productIndex = request.body.products.findIndex(
+                (requestBodyProduct) => requestBodyProduct.id === upProduct.id
+            )
+
+            const newProduct = {
+                id: upProduct.id,
+                name: upProduct.name,
+                price: upProduct.price,
+                category: upProduct.category.name,
+                url: upProduct.url,
+                quantity: request.body.products[productIndex].quantity
+            }
+
+            return newProduct
+        })
 
         const order = { // Informações do usuário chegando pelo token
             user: {
                 id: request.userId,
                 name: request.userName,
-            }
+            },
+            products: editedProduct
         }
 
-        return response.status(201).json(request.body)
+        return response.status(201).json(updatedProducts)
     }
 }
 
