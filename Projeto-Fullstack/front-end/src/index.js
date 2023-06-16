f5fcf62eb8469a03885a95f06fda6fce388b59b1
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-06-16 17:50:06 -0300

    Projeto Fullstack - Configuração dos Plugins do Eslint (Import helper e React hooks)

--- Projeto-Fullstack/front-end/src/index.js
+++ Projeto-Fullstack/front-end/src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 import Login from './containers/Login'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
    <Login />
-);
-
-
+)
