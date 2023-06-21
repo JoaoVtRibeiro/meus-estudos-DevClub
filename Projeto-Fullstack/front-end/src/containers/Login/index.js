85226ef80a9e51dcfb831ce239d23622c7a6741c
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-06-21 18:26:53 -0300

    Projeto Fullstack - Inicio da tela de Login

--- Projeto-Fullstack/front-end/src/containers/Login/index.js
+++ Projeto-Fullstack/front-end/src/containers/Login/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
+import { Container, Figure, Main, H1, Label, Input, Button, P } from './styles'
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <Figure>
+        <img></img>
+      </Figure>
+
+      <Main>
+        <Figure>
+          <img></img>
+        </Figure>
+
+        <H1>Login</H1>
+
+        <Label>Email</Label>
+        <Input></Input>
+
+        <Label>Senha</Label>
+        <Input></Input>
+
+        <Button>Entrar</Button>
+
+        <P>Não possui conta? <a>Sign Up</a></P>
+      </Main>
+    </Container>
   )
 }
 
