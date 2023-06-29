59050c0e4a16c23a9fab9e1d0906e10749d9980a
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-06-29 07:01:17 -0300

    Projeto Fullstack - Validação do formulário

--- Projeto-Fullstack/front-end/src/containers/Login/index.js
+++ Projeto-Fullstack/front-end/src/containers/Login/index.js
@@ -3,6 +3,9 @@
 import React from 'react'
 import { useForm } from 'react-hook-form' // Biblioteca react hook para tratamento de formulários
 
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as Yup from 'yup'
+
 import BurgerLogin from '../../assets/burger-login.png'
 import Logo from '../../assets/logo.png'
 
@@ -10,11 +13,20 @@ import { Container, BurgerLoginImage, Main, LogoImage, H1, Label, Input, Button,
 
 
 function Login() {
-  // Comandos e funções da biblioteca react-hook-form
+  // Yup (Validação dos campos)
+
+  const schema = Yup.object().shape({
+    email: Yup.string().email('Digite um email válido').required('Digite seu email'),
+    password: Yup.string().required('Digite sua senha').min(6, 'A senha deve ter no mínimo 6 digitos')
+  })
+
+  // Comandos e funções da biblioteca react-hook-form (Formulário)
   const { register, // Mapear os campos
     handleSubmit, // Enviar os dados
     formState: { errors } // Identificar os erros
-  } = useForm()
+  } = useForm({
+    resolver: yupResolver(schema) // Validar os campos
+  })
 
   const onSubmit = data => console.log(data)
 
@@ -31,12 +43,14 @@ function Login() {
 
         <H1>Login</H1>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form noValidate onSubmit={handleSubmit(onSubmit)}> {/* noValidate = não irá validar pelo html (sem mensagem de aviso do html nos campos) */}
           <Label>Email</Label>
           <Input type="email" {...register('email')}></Input>
+          <p>{errors.email?.message}</p> {/* mensagem de erro (validação pelo Yup) */}
 
           <Label id="label-password">Senha</Label>
           <Input type="password" {...register('password')}></Input>
+          <p>{errors.password?.message}</p>
 
           <Button type="submit">Entrar</Button>
         </form>
