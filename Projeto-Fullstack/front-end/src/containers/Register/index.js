e808a2bf0329fa15c99adfe46ae255df68c5f3df
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-07-04 08:09:24 -0300

    Projeto Fullstack - Inicio da tela de Cadastro do usuário

--- Projeto-Fullstack/front-end/src/containers/Register/index.js
+++ Projeto-Fullstack/front-end/src/containers/Register/index.js
@@ -11,18 +11,20 @@ import * as Yup from 'yup'
 import api from '../../services/api'
 
 // Estilizações e componentes
-import LoginImage from '../../assets/login-image.png'
+import RegisterImage from '../../assets/register-image.png'
 import Logo from '../../assets/logo.png'
 import Button from '../../components/Button'
-import { Container, BurgerLoginImage, Main, LogoImage, H1, Label, Input, ErrorMessage, P } from './styles'
+import { Container, BurgerRegisterImage, Main, LogoImage, H1, Label, Input, ErrorMessage, P } from './styles'
 
 
-function Register() {
+function Login() {
   // Yup (Validação dos campos)
 
   const schema = Yup.object().shape({
+    name: Yup.string().required('Digite seu nome'),
     email: Yup.string().email('Digite um email válido').required('Digite seu email'),
-    password: Yup.string().required('Digite sua senha').min(6, 'A senha deve ter no mínimo 6 digitos')
+    password: Yup.string().required('Digite sua senha').min(6, 'A senha deve ter no mínimo 6 digitos'),
+    confirmPassword: Yup.string().required('Confirme sua senha').min(6, 'A senha deve ter no mínimo 6 digitos')
   })
 
   // Comandos e funções da biblioteca react-hook-form (Formulário)
@@ -34,7 +36,8 @@ function Register() {
   })
 
   const onSubmit = async inputsClientData => { // inputsClientData = dados dos clientes recebidos pelos inputs
-    const response = await api.post('sessions', {
+    const response = await api.post('users', {
+      name: inputsClientData.name,
       email: inputsClientData.email,
       password: inputsClientData.password
     })
@@ -43,7 +46,7 @@ function Register() {
   return (
     <Container>
       <figure>
-        <BurgerLoginImage src={LoginImage} atl="register image"></BurgerLoginImage>
+        <BurgerRegisterImage src={RegisterImage} atl="login image"></BurgerRegisterImage>
       </figure>
 
       <Main>
@@ -51,24 +54,32 @@ function Register() {
           <LogoImage src={Logo} alt="logo image"></LogoImage>
         </figure>
 
-        <H1>Login</H1>
+        <H1>Cadastre-se</H1>
 
         <form noValidate onSubmit={handleSubmit(onSubmit)}> {/* noValidate = não irá validar pelo html (sem mensagem de aviso do html nos campos) */}
-          <Label>Email</Label>
-          <Input type="email" {...register('email')} error={errors.email?.message}></Input> {/* error = caso houver a mensagem, será true (para a estilização acontecer se necessário) */}
-          <ErrorMessage>{errors.email?.message}</ErrorMessage> {/* mensagem de erro (validação pelo Yup) */}
+          <Label>Nome</Label>
+          <Input type="text" {...register('name')} error={errors.name?.message}></Input> {/* error = caso houver a mensagem, será true (para a estilização acontecer se necessário) */}
+          <ErrorMessage>{errors.name?.message}</ErrorMessage> {/* mensagem de erro (validação pelo Yup) */}
 
-          <Label id="label-password">Senha</Label>
+          <Label id="email">Email</Label>
+          <Input type="email" {...register('email')} error={errors.email?.message}></Input>
+          <ErrorMessage>{errors.email?.message}</ErrorMessage>
+
+          <Label id="password">Senha</Label>
           <Input type="password" {...register('password')} error={errors.password?.message}></Input>
           <ErrorMessage>{errors.password?.message}</ErrorMessage>
 
-          <Button type="submit" style={{ marginTop: 60 }}>Entrar</Button>
+          <Label id="confirm-password">Confirmar Senha</Label>
+          <Input type="password" {...register('password')} error={errors.confirmpassword?.message}></Input>
+          <ErrorMessage>{errors.confirmpassword?.message}</ErrorMessage>
+
+          <Button type="submit" style={{ marginTop: 60 }}>Cadastrar</Button>
         </form>
 
-        <P>Não possui conta? <a>Criar conta</a></P>
+        <P>Já possui conta? <a>Fazer login</a></P>
       </Main>
     </Container>
   )
 }
 
-export default Register
+export default Login
---
+++ Projeto-Fullstack/front-end/src/containers/Register/styles.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components'
+
+// Divs
+
+export const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    overflow: hidden; /* impede barras de rolagem */
+`
+export const Main = styled.main`
+    background-color: #373737;
+    height: 100%;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    padding-left: 104px;
+    justify-content: center;
+
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+`
+
+// Elementos
+
+export const BurgerRegisterImage = styled.img`
+    height: 100%;
+`
+
+export const LogoImage = styled.img`
+    position: fixed;
+    top: 84px;
+    padding-left: 30px;
+`
+
+export const H1 = styled.h1`
+    color: #FFF;
+    font-size: 24px;
+    font-weight: 500;
+    text-align: center;
+    margin-top: 320px;
+    margin-bottom: 25px;
+    padding-right: 90px;
+`
+
+export const Label = styled.label`
+    color: #FFF;
+    font-size: 12px;
+    font-weight: 500;
+    &#label-password{
+        margin-top: 20px;
+    }
+`
+
+export const Input = styled.input`
+    width: 391px;
+    height: 38px;
+    border: ${ props => props.error ? '2px solid #CC1717' : 'none'}; // props : propriedades do elemento
+    border-radius: 5px;
+    box-shadow: 3px 3px 10px 0px rgba(74, 144, 226, 0.19);
+    margin-top: 7px;
+    padding-left: 12px;
+`
+
+export const ErrorMessage = styled.p `
+    color: #CC1717;
+    font-size: 14px;
+    font-weight: 400;
+    margin-top: 2px;
+`
+
+export const P = styled.p`
+    color: #FFF;
+    font-size: 14px;
+    margin-top: 30px;
+    a{
+        text-decoration: underline;
+        cursor: pointer;
+    }
+`
