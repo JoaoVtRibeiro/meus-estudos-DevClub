b600b7bb5adcc0a6e62b7413b5cda9431fba8547
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-07-17 12:04:22 -0300

    Projeto Fullstack - Inicio da tela da página Home

--- Projeto-Fullstack/front-end/src/containers/Home/index.js
+++ Projeto-Fullstack/front-end/src/containers/Home/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import HomeBanner from '../../assets/home-banner.png'
+import Categories from '../../assets/categories.png'
+import Offers from '../../assets/offers.png'
+import { Container, HomeImg } from './styles'
+
 function Home() {
-    return
+    return (
+        <Container>
+            <HomeImg src={HomeBanner} alt="banner da página"/>
+        </Container>
+    )
 }
 
 export default Home
