971ec77709ac87313d6dc51f6171747704d2467a
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-07-19 15:42:44 -0300

    Projeto Fullstack - Conexão do componente de categorias com a api

--- Projeto-Fullstack/front-end/src/components/CategoriesCarousel/index.js
+++ Projeto-Fullstack/front-end/src/components/CategoriesCarousel/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import api from '../../services/api'
 
 import Categories from '../../assets/categories.png'
 import { Container, CategoriesImg } from './styles'
 
 function CategoriesCarousel() {
+    useEffect(() =>{ // Sempre é executado quando: 1. A página é iniciada
+        async function loadCategories() {
+            const response = await api.get('categories')
+
+            console.log(response)
+        }
+
+        loadCategories()
+    }, []) // Sempre é executado quando: 2. Atualização de algum componente (que devem estar dentro do [])
+
     return (
         <Container>
             <CategoriesImg src={Categories} alt="logo das categorias"/>
