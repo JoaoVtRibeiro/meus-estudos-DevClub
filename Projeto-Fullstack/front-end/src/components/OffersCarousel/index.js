3dd19048f85fa804b772080ed44b316108303d03
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-07-25 07:45:17 -0300

    Projeto Fullstack - Carousel dos Produtos em Oferta

---
+++ Projeto-Fullstack/front-end/src/components/OffersCarousel/index.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import Carousel from 'react-elastic-carousel'
+
+import api from '../../services/api'
+
+import Offers from '../../assets/Offers.png'
+import { Container, OffersImg, CarouselItem, Image, Button } from './styles'
+
+function OffersCarousel() {
+    const [offers, setOffers] = useState([])
+
+    useEffect(() => { // Sempre é executado quando: 1. A página é iniciada
+        async function loadOffers() {
+            const { data } = await api.get('products') // apenas o "data" é necessário dos atributos do objeto
+
+            const onlyOffers = data.filter(product => product.offer)
+
+            setOffers(onlyOffers)
+        }
+
+        loadOffers()
+    }, []) // Sempre é executado quando: 2. Atualização de algum componente (que devem estar dentro do [])
+
+    const breakPoints = [ // "Pontos de quebra" indicando a partir de um valor de largura quantos itens serão mostrados (responsividade)
+        { width: 1, itemsToShow: 1 },
+        { width: 400, itemsToShow: 2 },
+        { width: 600, itemsToShow: 3 },
+        { width: 900, itemsToShow: 4 },
+        { width: 1300, itemsToShow: 5 }
+    ]
+
+    return (
+        <Container>
+            <OffersImg src={Offers} alt="logo das ofertas" breakPoints={breakPoints} />
+
+            <Carousel itemsToShow={4} style={{ width: '90%' }}>
+                {offers && offers.map(product => {
+                    <CarouselItem key={product.id}> {/* key = Permite identificação unica para cada item (Garantindo que o React irá detectar quaisquer alteração em cada um deles)  */}
+                        <Image src={product.url} alt="foto da produto em oferta"></Image>
+                        <Button>{product.name}</Button>
+                    </CarouselItem>
+                })}
+            </Carousel>
+        </Container>
+    )
+}
+
+export default OffersCarousel
--- Projeto-Fullstack/front-end/src/containers/Home/index.js
+++ Projeto-Fullstack/front-end/src/containers/Home/index.js
@@ -3,12 +3,14 @@ import React from 'react'
 import HomeBanner from '../../assets/home-banner.png'
 import { Container, HomeImg } from './styles'
 import CategoriesCarousel from '../../components/CategoriesCarousel'
+import OffersCarousel from '../../components/OffersCarousel'
 
 function Home() {
     return (
         <Container>
             <HomeImg src={HomeBanner} alt="banner da página" />
             <CategoriesCarousel />
+            <OffersCarousel />
         </Container>
     )
 }
