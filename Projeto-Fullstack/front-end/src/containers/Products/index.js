5886e93a80a5997b671563fe59d78fab5d6605e4
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-07-30 21:41:38 -0300

    Projeto Fullstack - Menu de categorias

--- Projeto-Fullstack/front-end/src/containers/Products/index.js
+++ Projeto-Fullstack/front-end/src/containers/Products/index.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import api from '../../services/api'
 
 import ProductsBanner from '../../assets/products-banner.png'
 import { Container, ProductsImg } from './styles'
 
 function Products() {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => { // Sempre é executado quando: 1. A página é iniciada
+        async function loadCategories() {
+            const { data } = await api.get('categories') // apenas o "data" é necessário dos atributos do objeto
+
+            const addAllCategories = [{ id: 0, name: 'Todas'}, ...data]
+
+            setCategories(addAllCategories)
+        }
+
+        loadCategories()
+    }, [])
+
     return (
         <Container>
             <ProductsImg src={ProductsBanner} alt="banner da página" />
-        </Container>
+            {categories && categories.map(category => (
+                <button key={category.id}>{category.name}</button>
+            ))}
+        </Container >
     )
 }
 
