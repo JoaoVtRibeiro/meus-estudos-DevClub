54da1434ba550bd9e9e6d81a773f39c7c81f9a4a
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-08-04 07:52:40 -0300

    Projeto Fullstack - Estrutura dos Cards de produtos

--- Projeto-Fullstack/front-end/src/components/CardProduct/index.js
+++ Projeto-Fullstack/front-end/src/components/CardProduct/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
-function CardProduct() {
-    return <div>Hello</div>
+import { Container, Image, ProductName, ProductPrice } from './style'
+import Button from '../Button'
+
+function CardProduct({ product }) {
+    return (
+        <Container>
+            <Image src={product.url} alt="imagem do produto" />
+            <ProductName>{product.name}</ProductName>
+            <ProductPrice>{product.price}</ProductPrice>
+            <Button>Adicionar</Button>
+        </Container>
+    )
 }
 
 export default CardProduct
