e87ed978b2716029aa1818b1536b0adae3148b31
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-08-27 17:20:03 -0300

    Projeto Fullstack - Estilização do Resumo do carrinho

--- Projeto-Fullstack/front-end/src/components/CartResume/index.js
+++ Projeto-Fullstack/front-end/src/components/CartResume/index.js
@@ -7,19 +7,19 @@ export function CartResume() {
     return (
         <>
             <Container>
-                <div>
-                    <h2>Resumo do pedido</h2>
-                    <p>Itens</p>
-                    <p>R$ 10,00</p>
-                    <p>Taxa de entrega</p>
-                    <p>R$ 10,00</p>
+                <div className='container-top'>
+                    <h2 className='title'>Resumo do pedido</h2>
+                    <p className='items'>Itens</p>
+                    <p className='items-price'>R$ 10,00</p>
+                    <p className='delivery-tax'>Taxa de entrega</p>
+                    <p className='delivery-tax-price'>R$ 10,00</p>
                 </div>
-                <div>
+                <div className='container-bottom'>
                     <p>Total</p>
                     <p>R$ 20,00</p>
                 </div>
             </Container>
-            <Button>Finalizar Pedido</Button>
+            <Button style={{width: '100%', marginTop: 30}}>Finalizar Pedido</Button>
         </> /* Fragment, para fazer com que o botão assuma o tamanho total que ele tem disponivel (não ser alterado pela estilização do container)  */
     )
 }
