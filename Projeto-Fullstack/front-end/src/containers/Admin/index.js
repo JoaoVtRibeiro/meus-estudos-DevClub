6ba9ae621a97b167593200a078c01907a5080acc
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-09-29 17:13:14 -0300

    Projeto Fullstack - Inicio do menu lateral e estilização da tela de Admin

--- Projeto-Fullstack/front-end/src/containers/Admin/index.js
+++ Projeto-Fullstack/front-end/src/containers/Admin/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import { Orders } from './Orders'
 import { Container } from '/style.js'
+import { SideMenuAdmin } from '../../components/SideMenuAdmin'
 
 export function Admin() {
-    return(
+    return (
         <Container>
+            <SideMenuAdmin />
             <Orders />
         </Container>
     )
