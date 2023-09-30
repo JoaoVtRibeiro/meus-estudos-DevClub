803326d002fea6939da65ec28dc6d10ae8ed73b8
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-09-30 17:01:37 -0300

    Projeto Fullstack - Estrutura do menu lateral (Página de administração)

--- Projeto-Fullstack/front-end/src/components/SideMenuAdmin/index.js
+++ Projeto-Fullstack/front-end/src/components/SideMenuAdmin/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-import { Container } from './styles'
+import listLinks from './menu-list'
+import { Container, ItensContainer, ListLink } from './styles'
 
 export function SideMenuAdmin() {
     return (
         <Container>
-
+            <hr></hr>
+            {listLinks.map(item => {
+                <ItensContainer key={item.id}>
+                    <ListLink to={item.link}>{item.label}</ListLink>
+                </ItensContainer>
+            })}
         </Container>
     )
 }
--- Projeto-Fullstack/front-end/src/components/SideMenuAdmin/styles.js
+++ Projeto-Fullstack/front-end/src/components/SideMenuAdmin/styles.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-export const Container = styled.div`
-   
-`
+export const Container = styled.div``
+
+export const ItensContainer = styled.div``
+
+export const ListLink = styled(Link)``
