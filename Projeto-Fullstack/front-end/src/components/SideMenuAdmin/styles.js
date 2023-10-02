15643c785e906b2cc0b33231136b8a64bcee61b3
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-10-02 19:42:20 -0300

    Projeto Fullstack - Estilização do menu lateral

--- Projeto-Fullstack/front-end/src/components/SideMenuAdmin/styles.js
+++ Projeto-Fullstack/front-end/src/components/SideMenuAdmin/styles.js
@@ -1,8 +1,35 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-export const Container = styled.div``
+export const Container = styled.div`
+    background: #3c3c3c;
+    box-shadow: 0px 0px 14px rgba(0, 0, 0, 0.15);
+    width: 300px;
+    top: 0;
+    left: 0;
 
-export const ItensContainer = styled.div``
+    hr{
+        margin: 50px 15px;
+    }
+`
 
-export const ListLink = styled(Link)``
+export const ItensContainer = styled.div`
+    height: 60px;
+    display: flex;
+    align-items: center;
+    background: #565656;
+    border-radius: 2px;
+    margin: 8px;
+
+    .icon{
+        color: #FFFFFF;
+    }
+`
+
+export const ListLink = styled(Link)`
+    font-size: 16px;
+    line-height: 19px;
+    color: #ffffff;
+    text-decoration: none;
+    margin-left: 8px;
+`
