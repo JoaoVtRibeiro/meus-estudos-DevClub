41b7cd09b1d46f6e2d7bf318cb556fa1d5765859
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-10-03 18:06:21 -0300

    Projeto Fullstack - Estrutura e estilização do botão de logout

--- Projeto-Fullstack/front-end/src/components/SideMenuAdmin/index.js
+++ Projeto-Fullstack/front-end/src/components/SideMenuAdmin/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import LogoutIcon from '@mui/icons-material/Logout'
+
 import listLinks from './menu-list'
 import { Container, ItensContainer, ListLink } from './styles'
 
@@ -8,11 +10,16 @@ export function SideMenuAdmin() {
         <Container>
             <hr></hr>
             {listLinks.map(item => (
-                <ItensContainer key={item.id}>
+                <ItensContainer key={item.id} isActive={true}>
                     <item.icon className="icon" />
                     <ListLink to={item.link}>{item.label}</ListLink>
                 </ItensContainer>
             ))}
+            <hr></hr>
+            <ItensContainer>
+                <LogoutIcon style={{ position: 'absolute', bottom: '30px'}}/>
+                <ListLink style={{ color: '#FFFFFF' }}>Sair</ListLink>
+            </ItensContainer>
         </Container>
     )
 }
