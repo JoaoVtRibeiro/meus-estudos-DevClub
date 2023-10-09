05444fdaa555fe0dda7b81854aec8ea78c811855
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-10-09 13:58:29 -0300

    Projeto Fullstack - Organização da renderização de cada item do menu lateral (tela admin)

--- Projeto-Fullstack/front-end/src/components/SideMenuAdmin/menu-list.js
+++ Projeto-Fullstack/front-end/src/components/SideMenuAdmin/menu-list.js
@@ -1,12 +1,33 @@
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import SellIcon from '@mui/icons-material/Sell';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import paths from '../../constants/path';
 
 const listLinks = [
     {
         id: 1,
         label: 'Pedidos',
-        link: '/pedidos',
+        link: paths.OrderAdm,
+        icon: SellIcon
+    },
+    {
+        id: 2,
+        label: 'Lista de Produtos',
+        link: paths.ListProductsAdm,
+        icon: ShoppingCartIcon
+    } /*  
+    {
+        id: 3,
+        label: 'Editar Produto',
+        link: paths.EditProductsAdm,
         icon: ShoppingBagIcon
-    }
+    },
+    {
+        id: 4,
+        label: 'Novo Produto',
+        link: paths.NewProductAdm,
+        icon: ShoppingBagIcon
+    }*/
 ]
 
 export default listLinks
--- Projeto-Fullstack/front-end/src/containers/Admin/index.js
+++ Projeto-Fullstack/front-end/src/containers/Admin/index.js
@@ -4,16 +4,21 @@ import { SideMenuAdmin } from '../../components/SideMenuAdmin'
 import { Orders } from './Orders'
 import { ListProducts } from './ListProducts'
 import { Container, ContainerItems } from '/style.js'
+import paths from '../../constants/path'
 
 
-export function Admin() {
+export function Admin({ math: { path } }) { // props dos componentes em components/SideMenuAdmin (caso aja duvida: console.log(props)) *obs fim da página
     return (
         <Container>
             <SideMenuAdmin />
             <ContainerItems>
-                {/* <Orders /> */}
-                <ListProducts />
+                {path === paths.OrderAdm && <Orders /> } {/* Se path for igual a url então renderizar componente */}
+                {path === paths.OrderAdm && <ListProducts /> }   
             </ContainerItems>
         </Container>
     )
 }
+
+/* 
+    Obs: Esses props são um padrão de todo componente
+*/
