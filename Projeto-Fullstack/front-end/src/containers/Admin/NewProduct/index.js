e4bd1ccf185f1402863d71516371be5952c0dd1d
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-10-13 16:21:33 -0300

    Projeto Fullstack - Formulário de adição de produtos

--- Projeto-Fullstack/front-end/src/containers/Admin/NewProduct/index.js
+++ Projeto-Fullstack/front-end/src/containers/Admin/NewProduct/index.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import ReactSelect from 'react-select'
 
 import api from '../../../services/api'
-import { Container } from './style'
+import Button from '../../../components'
+import { Container, Label, Input } from './style'
 
 function NewProduct() {
+    const { register, handleSubmit } = useForm()
+    const onSubmit = data => console.log(data)
+
+
     useEffect(() => {
         async function loadOrders() {
             const { data } = await api.get('products')
@@ -14,7 +21,20 @@ function NewProduct() {
 
     return (
         <Container>
-            
+            <form noValidate>
+                <Label for="name">Nome</Label>
+                <Input id="name" type="text" {...register('name')} />
+
+                <Label for="name">Preço</Label>
+                <Input id="name" type="number" {...register('price')} />
+
+                <Label for="name">Upload da imagem</Label>
+                <Input id="name" type="file" accept="image/png, image/jpeg" />
+
+                <ReactSelect />
+
+                <Button></Button>
+            </form>
         </Container>
     )
 }
--- Projeto-Fullstack/front-end/src/containers/Admin/NewProduct/style.js
+++ Projeto-Fullstack/front-end/src/containers/Admin/NewProduct/style.js
@@ -4,3 +4,7 @@ export const Container = styled.div`
     width: 100%;
     height: 100%;
 `
+
+export const Label = styled.label``
+
+export const Input = styled.input``
