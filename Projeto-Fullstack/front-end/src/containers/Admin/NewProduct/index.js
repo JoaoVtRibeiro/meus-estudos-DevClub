b83639fabe9442251cf995aa7c2330ea36bf4b19
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-10-19 17:12:07 -0300

    Projeto Fullstack - Configurando o Controller para o React Select (input controlado)

--- Projeto-Fullstack/front-end/src/containers/Admin/NewProduct/index.js
+++ Projeto-Fullstack/front-end/src/containers/Admin/NewProduct/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, Controller } from 'react-hook-form'
 import ReactSelect from 'react-select'
 
 import UploadFileIcon from '@mui/icons-material/UploadFile';
@@ -9,9 +9,9 @@ import { Container, Label, Input, LabelUpload, ButtonStyles } from './style'
 function NewProduct() {
     const [fileName, setFileName] = useState(null)
     const [categories, setCategories] = useState([])
-    const { register, handleSubmit } = useForm()
-    const onSubmit = data => console.log(data)
+    const { register, handleSubmit, control } = useForm()
 
+    const onSubmit = data => console.log(data)
 
     useEffect(() => {
         async function loadCategories() {
@@ -23,7 +23,7 @@ function NewProduct() {
 
     return (
         <Container>
-            <form noValidate>
+            <form noValidate onSubmit={handleSubmit(onSubmit)}>
                 <Label for="name">Nome</Label>
                 <Input id="name" type="text" {...register('name')} />
 
@@ -47,17 +47,47 @@ function NewProduct() {
                     />
                 </LabelUpload>
 
-                <ReactSelect 
-                    options={categories} // De onde será tirado as opções
-                    getOptionLabel={category => category.name} // O que será mostrado
-                    getOptionValue={category => category.id} // Valor que será enviado
-                    placeholder="Categoria"
-                />
+                <Controller
+                    name="category_id"
+                    control={control}
+                    render={({ field }) => {
+                        return (
+                            <ReactSelect
+                                {...field}
+                                options={categories} // De onde será tirado as opções
+                                getOptionLabel={category => category.name} // O que será mostrado
+                                getOptionValue={category => category.id} // Valor que será enviado 
+                                placeholder="Categoria"
+                            />
+                        )
+                    }}
+                ></Controller>
 
                 <ButtonStyles>Adicionar Produto</ButtonStyles>
             </form>
-        </Container>
+        </Container >
     )
 }
 
 export default NewProduct
+
+
+/* 
+    Inputs
+
+    Não controlados => Eles não são controlados por NINGUÉM, se viram sozinhos
+    Ex: Um input que ele próprio guarda o seu valor (Auto-suficiente)
+
+    Controlados => Eles são controlados por algum outro componente ou não são auto-suficientes
+    Ex: Um input que não guarda o valor dele próprio, ele necessita guardar em uma variável
+
+
+    Ambos os exemplos podem ser encontrados em front-end/src/containers/Admin/NewProducts/index.js
+        - React Select: Controlado
+        - Demais inputs: Não controlados
+
+    Obs: React Hook form não funciona com inputs de terceiros (Como o react select), utilize o Controller
+
+        - Controller: Faz uma interface entre o React hook form e o componente, no caso o React select (faz um "meio de campo" entre os dois)
+
+*/
