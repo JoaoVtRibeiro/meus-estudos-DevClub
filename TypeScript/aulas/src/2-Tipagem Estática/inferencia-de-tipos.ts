71dae694ea9c29ce3555c446ee07acef8b19935c
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-11-07 15:38:25 -0300

    TypeScript - Inferência de tipos

---
+++ TypeScript/aulas/src/2-Tipagem Estática/inferencia-de-tipos.ts
@@ -0,0 +1,13 @@
+/* 
+    Inferência de tipos
+
+    A inferência de tipos no TypeScript é um recurso que permite que o compilador (no caso, o typescript)
+    determine automaticamente o tipo de uma variável com base no valor atribuído a ela.
+
+    Isso significa que você não precisa especificar explicitamente o tipo de cada variável, 
+    o compilador irá estabelecer o tipo com base no contexto em que a variável é usada. 
+    Isso torna o código mais conciso e menos propenso a erros de tipo.
+*/
+
+// Exemplo
+let animal = 'cat' // Ao passar o mouse em cima do nome (animal), o typescript irá mostrar que ele já entende que essa variavel será uma string
