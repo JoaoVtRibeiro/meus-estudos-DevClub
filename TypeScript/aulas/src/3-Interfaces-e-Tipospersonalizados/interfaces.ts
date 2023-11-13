30e271545c99b53caf79814d67521d81ef6b458a
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-11-13 16:08:03 -0300

    TypeScript - Interface (explicação e descrições)

--- TypeScript/aulas/src/3-Interfaces-e-Tipospersonalizados/interfaces.ts
+++ TypeScript/aulas/src/3-Interfaces-e-Tipospersonalizados/interfaces.ts
@@ -1,20 +1,25 @@
 /* 
     Interfaces
 
+        Funcionam de forma muito parecida com o "type", porém a diferente entre eles é:
+        type - é utilizado para personalizar objetos mais comuns
+        interface - utilizado para personalizar objetos com implementações (como uma função)
+
+        No fim, ambos funcionam da mesma forma como div e main por exemplo, mas existe essa diferença por semântica
 */
 
 interface Car {
     brand: string;
     model: string;
     year: number;
-    startedEngine: () => void;
+    startedEngine: () => void; // Função para receber a implementação
 }
 
 const myCar: Car = {
     brand: 'Audi',
     model: 'A3',
     year: 2023,
-    startedEngine: () => {
+    startedEngine: () => { // Implementação
         console.log('Engine started')
     }
 }
