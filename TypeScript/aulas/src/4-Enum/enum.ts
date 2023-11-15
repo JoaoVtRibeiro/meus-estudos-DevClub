d36a81bfb2267500240d1c64dfa41487e1de5ddb
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-11-15 15:31:36 -0300

    TypeScript - Enum (Descrição e anotações)

--- TypeScript/aulas/src/4-Enum/enum.ts
+++ TypeScript/aulas/src/4-Enum/enum.ts
@@ -1,14 +1,23 @@
 /* 
     Enum
+
+        É um "facilitador", que permite organizar e também evitar
+        erros de escrita de valores de propriedades, criando um conjunto de opções
+
+        Por exemplo: imaginado que você tem uma lista de cores,
+        vermelho, azul, verde. Em vez de usar as palavras diretamente, 
+        você pode criar um enum chamado "Cores" e colocar essas opções lá dentro. 
+        Assim, quando você quiser usar uma cor e ter certeza que é exatamente esse valor que será inserido/escolhido
+        você pode simplesmente escrever o enum "Cores"
 */
 
-const gender = {
+const gender = { // Exemplo de um objeto com esse mesmo proposito no javascript
     male: 'male',
     female: 'female',
     other: 'other'
 }
 
-enum Gender {
+enum Gender { // Exemplo do enum
     MALE = 'Male',
     FEMALE = 'Female',
     OTHER = 'Other'
@@ -17,10 +26,11 @@ enum Gender {
 Gender.FEMALE
 
 
+// Exclusividade do enum
 type Person = {
     name: string;
     age: number;
-    gender: Gender;
+    gender: Gender; // Filtrar os valores possiveis de uma propriedade
 }
 
 const person: Person = {
