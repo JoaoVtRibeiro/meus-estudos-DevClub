dbf19ed5f8790193e343c47e8a7fb731198d408c
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-11-16 01:43:56 -0300

    TypeScript - Generics (Exemplo)

---
+++ TypeScript/aulas/src/5-Generics/generics.ts
@@ -0,0 +1,20 @@
+/* 
+    Generics
+
+ */
+
+function getFirstElement<T>(data: T[]): T | null {
+    if (!data.length) {
+        return null
+    }
+
+    const [firstElement] = data
+
+    return firstElement
+}
+
+const stringArray: string[] = ['Lucas', 'Fernando', 'Gabriel', 'Maria']
+const numberArray: number[] = [1, 2, 3, 4, 5, 6]
+
+console.log(getFirstElement<string>(stringArray))
+console.log(getFirstElement<number>(numberArray))
