3e5e5e1cfd66fe15853636ce8d75c104fcbc6d30
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-11-19 13:40:47 -0300

    TypeScript - Utility Types (descrição)

--- TypeScript/aulas/src/6-Utility-Types/utility-types.ts
+++ TypeScript/aulas/src/6-Utility-Types/utility-types.ts
@@ -1,3 +1,15 @@
+/* 
+    Utility Types
+
+        Os utility types são tipos pré-definidos no TypeScript que fornecem funcionalidades
+        extras para manipulação de tipos. Eles permitem que você transforme, combine e manipule 
+        tipos de forma mais fácil e eficiente. Alguns exemplos de utility types incluem:
+
+        - Partial
+        - Omit
+        - Pick
+*/
+
 type User = {
     id: number;
     username: string;
