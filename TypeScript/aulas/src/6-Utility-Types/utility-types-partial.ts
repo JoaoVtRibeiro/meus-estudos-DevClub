0607b6b2ab1a6505d87f42097d6ced490211198e
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-11-20 14:01:15 -0300

    TypeScript - Utility Types, Partial (Descriação e exemplos)

--- TypeScript/aulas/src/6-Utility-Types/utility-types-partial.ts
+++ TypeScript/aulas/src/6-Utility-Types/utility-types-partial.ts
@@ -5,12 +5,45 @@
         extras para manipulação de tipos. Eles permitem que você transforme, combine e manipule 
         tipos de forma mais fácil e eficiente. Alguns exemplos de utility types incluem:
 
-        - Partial
+        - Partial (Nativo do TypeScript)
+            Permite criar um "tipo parcial", que é basicamente 
+            um tipo que não precisa necessáriamente de todas as propriedades
+
         - Omit
         - Pick
 */
 
-type User = {
+// Exemplo Partial
+
+type User1 = {
+    id: number;
+    username: string;
+    password: string;
+}
+
+const userPartial1: User1 = { // usuario com todos os dados
+    id: 1,
+    username: 'agustinhopneto',
+    password: 'typescript123',
+}
+
+type PartialUser = Partial<User1>
+
+
+const userPartial2: PartialUser = { // usuario com dados parciais
+    id: 2,
+    username: 'joao'
+}
+
+const userPartial3: PartialUser = {
+    username: 'vitor',
+    password: '123typescript',
+}
+
+
+/* 
+
+type User1 = {
     id: number;
     username: string;
     password: string;
@@ -26,7 +59,7 @@ const user: User = {
     updatedAt: new Date()
 }
 
-type PartialUser = Partial<User>
+
 
 type NewUser = Omit<User, 'id' | 'username'>
 
@@ -35,3 +68,5 @@ type SearchUser = Pick<User, 'username'>
 const searchUser: SearchUser = {
     username: 'agustinhopneto'
 }
+
+ */
