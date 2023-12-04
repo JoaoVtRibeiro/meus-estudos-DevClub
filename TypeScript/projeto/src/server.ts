d33f1b0e2c6fd0ef844d62b555d9a34be4371047
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-12-04 06:04:03 -0300

    TypeScript Projeto - Lembrete em resumo das etapas do projeto

--- TypeScript/projeto/src/server.ts
+++ TypeScript/projeto/src/server.ts
@@ -15,10 +15,13 @@ app.listen(3333, () => {
     Arquitetura dos caminhos do projeto
 
         Request:
-
             Entity -> Service -> Controller -> Route
 
         Response:
-
             Route -> Controller -> Service -> Entity
+
+            Entity: Modelagem os dados, o "coração" do projeto (Não necessáriamente só os dados do banco, mas também auxiliares)
+            Service: Responsavel pelas regras de negócio (quem faz a roda girar)
+            Controller: Gerencia as requisições/Saber quem chamar a partir da requisição (ex: qual service ou método)
+            Route: As Rotas, definem a url, são onde a resposta final para o usúario é enviada e onde as requisições são feitas
 */
