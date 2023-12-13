c37ba7bb2d2197b684249d7a8d11ca0b8fa419c1
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-12-13 15:57:35 -0300

    TypeScript Projeto - Definição "uppercase" para o atributo de cor

--- TypeScript/projeto/src/entities/category.entity.ts
+++ TypeScript/projeto/src/entities/category.entity.ts
@@ -9,10 +9,10 @@ export class Category{
     public title: string;
     public color: string;
 
-    constructor(props: CategoryProps){
-        this.id = props.id
-        this.title = props.title
-        this.color = props.color
+    constructor({ id , color, title}: CategoryProps){
+        this.id = id
+        this.title = title
+        this.color = color.toUpperCase()
     }
 }
 
