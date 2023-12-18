6c4c75b9c992226312447811c5a6aaa494d8e1f6
jvribeirodev <joaoribeiro.cpu@gmail.com>
2023-12-18 11:37:32 -0300

    TypeScript Projeto -  Correção do erro da variavel de ambiente

--- TypeScript/projeto/src/database/index.ts
+++ TypeScript/projeto/src/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-export async function setupMong(): Promise<void> {
+export async function setupMongo(): Promise<void> {
     try {
         if (mongoose.connection.readyState === 1) {
             return
@@ -8,7 +8,7 @@ export async function setupMong(): Promise<void> {
 
 
         console.log('Connecting to DB...')
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL as string)
         console.log('DB Connected!')
     } catch {
         throw new Error(' DB not connected')
