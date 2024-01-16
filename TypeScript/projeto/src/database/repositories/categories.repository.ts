3d3c004dfb5a4b09291a36ff16d01635213cc53b
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-01-16 07:11:31 -0300

    TypeScript Projeto - Correção da identação do arquivo do repositorio das categorias

--- TypeScript/projeto/src/database/repositories/categories.repository.ts
+++ TypeScript/projeto/src/database/repositories/categories.repository.ts
@@ -2,10 +2,10 @@ import { Category } from '../../entities/category.entity'
 import { CategoryModel } from '../schemas/category.schema'
 
 export class CategoriesRepository {
-    constructor(private model: typeof CategoryModel) {}
+    constructor(private model: typeof CategoryModel) { }
 
-    async create({ title, color}: Category) {
-        const createdCategory = await this.model.create({ title, color})
+    async create({ title, color }: Category) {
+        const createdCategory = await this.model.create({ title, color })
 
         return createdCategory.toObject<Category>()
     }
