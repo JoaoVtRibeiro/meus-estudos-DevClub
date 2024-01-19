360252a455cf38fb6b61f4b8a0ce5505db8efa72
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-01-19 19:43:34 -0300

    TypeScript Projeto - Especificando o método de persistência de dados

--- TypeScript/projeto/src/database/repositories/categories.repository.ts
+++ TypeScript/projeto/src/database/repositories/categories.repository.ts
@@ -4,7 +4,7 @@ import { CategoryModel } from '../schemas/category.schema'
 export class CategoriesRepository {
     constructor(private model: typeof CategoryModel) { }
 
-    async create({ title, color }: Category) {
+    async create({ title, color }: Category) { // método para criar os objetos dentro do banco (método da persistência de dados)
         const createdCategory = await this.model.create({ title, color })
 
         return createdCategory.toObject<Category>()
