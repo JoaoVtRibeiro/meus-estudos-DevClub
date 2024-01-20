37618351e32c6b3a3e33f3a8456b3735fbd557b6
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-01-20 20:51:55 -0300

    TypeScript Projeto - Categories Repository, descrições finalizadas

--- TypeScript/projeto/src/database/repositories/categories.repository.ts
+++ TypeScript/projeto/src/database/repositories/categories.repository.ts
@@ -2,11 +2,14 @@ import { Category } from '../../entities/category.entity'
 import { CategoryModel } from '../schemas/category.schema'
 
 export class CategoriesRepository {
-    constructor(private model: typeof CategoryModel) { }
+    constructor(private model: typeof CategoryModel) { } // typeof definindo que o model da classe será "do tipo" do model criado no category.schema
 
-    async create({ title, color }: Category) { // método para criar os objetos dentro do banco (método da persistência de dados)
-        const createdCategory = await this.model.create({ title, color })
+    async create({ title, color }: Category): Promise<Category> { // método para criar as categorias dentro do banco
+        const createdCategory = await this.model.create({ title, color }) // por conta do model, será possivel acessar os métodos de persistencia de dados do mongodb, como o create
 
-        return createdCategory.toObject<Category>()
+        return createdCategory.toObject<Category>() 
+        // sem o "toObject", será retornado apenas um objeto de documento do mongo, como será um json da resposta, além disso ele recebe uma tipagem genérica, no caso o Category
     }
 }
+
+// Lembrando que o model que permite a conexão com o banco
--- TypeScript/projeto/src/entities/category.entity.ts
+++ TypeScript/projeto/src/entities/category.entity.ts
@@ -1,22 +1,22 @@
 type CategoryProps = {
-    id?: string;
+    _id?: string; // Os _Id do mongo são salvos com um "_" na frente
     title: string;
     color: string;
 }
 
 export class Category{
-    public id?: string;
+    public _id?: string;
     public title: string;
     public color: string;
 
-    constructor({ id , color, title}: CategoryProps){
-        this.id = id
+    constructor({ _id , color, title}: CategoryProps){
+        this._id = _id
         this.title = title
         this.color = color.toUpperCase() // Padronizando os dados para serem todos em maiúsculo
     }
 }
 
-const category = new Category({id: 'id-da-instancia', title:'title', color: '#ff33bb'}) // Instanciando a classe
+const category = new Category({_id: '_id-da-instancia', title:'title', color: '#ff33bb'}) // Instanciando a classe
 
 
 /* 
