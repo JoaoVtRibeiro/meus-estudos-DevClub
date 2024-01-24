f003f13d095c0a29cbd1a968e09ae8da02aa5a3b
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-01-24 11:58:51 -0300

    TypeScript Projeto - Dto, Descrição

--- TypeScript/projeto/src/dtos/categories.dtos.ts
+++ TypeScript/projeto/src/dtos/categories.dtos.ts
@@ -2,3 +2,15 @@ export type CreateCategoryDTO = {
     title: string;
     color: string;
 }
+
+
+/* 
+    DTO - Data Transfer Object / Objeto de Transferência de Dados
+
+        A função de um dto é basicamente, definir quais tipos serão recebidos para poder criar/listar uma categoria
+        ele não é relacionado a nenhuma entidade ou algo do tipo, tem haver com a propria api, com o que o usuario envia.
+        É simplismente fazer um tipo customizado para saber o que será passado, porque quando os dados são enviados pela api,
+        nem sempre vão ser os mesmo tipos dados de uma entidade por exemplo.
+
+        É uma boa pratica, que ajuda na validação dos dados
+*/
