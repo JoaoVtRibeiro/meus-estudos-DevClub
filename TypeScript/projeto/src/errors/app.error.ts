4c0ad3def00cb8216d0bf53dda5d185945889ec2
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-02-03 05:28:15 -0300

    TypeScript Projeto -  Error Handler (Middleware)

--- TypeScript/projeto/src/errors/app.error.ts
+++ TypeScript/projeto/src/errors/app.error.ts
@@ -1,4 +1,4 @@
-import { StatusCodes } from "http-status-codes" // Semantica*anotar (Ele é basicamente um enum)
+import { StatusCodes } from 'http-status-codes' // Semantica*anotar (Ele é basicamente um enum)
 
 export class AppError extends Error { // Herança*anotar
     public statusCode: StatusCodes
