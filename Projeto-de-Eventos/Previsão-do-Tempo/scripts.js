b444e5bfeb363f5a27cb1f092d5d7b22b93fd8e3
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-03-07 09:54:19 -0300

    Projeto Previsão do Tempo - Logíca do Input, Button e Informações do Clima chegando pela API

---
+++ Projeto-de-Eventos/Previsão-do-Tempo/scripts.js
@@ -0,0 +1,24 @@
+let apiKey = "e6da02c324eea071e268344db4ca344c"
+
+function displayOnScreen(data){
+    
+}
+
+async function searchCity(city) {
+    let apiData = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" + // Endereço da Api
+        city + // Nome da Cidade
+        "&appid=" + apiKey + // Verificador da chave + chave
+        "&lang=pt_br" + // Idioma das informações
+        "&units=metric" // Por padrão as unidades vem em kelvin, metric converte para celsius
+    )
+    .then(response => response.json())
+
+    displayOnScreen(apiData)
+}
+
+function getCityName() {
+    let inputCityName = document.querySelector("input").value
+
+    searchCity(inputCityName)
+}
