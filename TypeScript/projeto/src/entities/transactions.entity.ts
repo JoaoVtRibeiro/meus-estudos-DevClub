aa9235b30780b6d17fec42a3fedde42ac927df6c
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-03-18 00:27:00 -0300

    TypeScript Projeto - Definições do tipo de transação

--- TypeScript/projeto/src/entities/transactions.entity.ts
+++ TypeScript/projeto/src/entities/transactions.entity.ts
@@ -1,8 +1,14 @@
 import { Category } from './category.entity'
 
+export enum TransactionType {
+    INCOME = 'income',
+    EXPENSE = 'expanse',
+}
+
 type TransactionProps =  {
     _id?: string;
     amount: number;
     date: Date;
     category: Category;
+    type: TransactionType;
 }
