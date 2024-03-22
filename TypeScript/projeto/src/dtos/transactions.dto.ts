b2c42798b9e1e0812091282f79b2012c5dbbee76
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-03-22 00:01:03 -0300

    TypeScript Projeto - zObject e zInfer

--- TypeScript/projeto/src/dtos/transactions.dto.ts
+++ TypeScript/projeto/src/dtos/transactions.dto.ts
@@ -1,10 +1,13 @@
 import { z } from 'zod'
 import { TransactionType } from '../entities/transactions.entity'
 
-export const createTransactionsSchema = {
+export const createTransactionSchema = {
     title: z.string(),
     amount: z.number().int().positive(),
     type: z.nativeEnum(TransactionType),
     data: z.coerce.date(),
     categoryId: z.string(),
 }
+
+const createTransactionObject = z.object(createTransactionSchema)
+export type createTransactionDTO = z.infer<typeof createTransactionObject>
