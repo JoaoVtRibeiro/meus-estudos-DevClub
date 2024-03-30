69ffaef84997db8fa68cbdb8585532c3b8847857
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-03-30 04:36:39 -0300

    TypeScript Projeto - Finalização do Service das transactions

--- TypeScript/projeto/src/services/transactions.services.ts
+++ TypeScript/projeto/src/services/transactions.services.ts
@@ -1,7 +1,9 @@
+import { StatusCodes } from 'http-status-codes'
 import { CategoriesRepository } from '../database/repositories/categories.repository'
 import { TransactionsRepository } from '../database/repositories/transactions.repository'
 import { CreateTransactionDTO } from '../dtos/transactions.dto'
 import { Transaction } from '../entities/transactions.entity'
+import { AppError } from '../errors/app.error'
 
 export class TransactionService {
     constructor(
@@ -11,5 +13,21 @@ export class TransactionService {
 
     async create({title, type, date, categoryId, amount}: CreateTransactionDTO): Promise<Transaction> {
         const category = await this.categoriesRepository.findById(categoryId)
+
+        if(!category) {
+            throw new AppError('Category does not exist', StatusCodes.NOT_FOUND)
+        }
+
+        const transaction = new Transaction({
+            title,
+            type,
+            date,
+            category,
+            amount
+        })
+
+        const createTransaction = await this.transactionsRepository.create(transaction)
+
+        return createTransaction
     }
 }
