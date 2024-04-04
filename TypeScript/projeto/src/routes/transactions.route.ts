f14aea8ccd736420bf7d3812bc371424262ae685
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-04-04 06:27:44 -0300

    TypeScript Projeto - Controller e Factory instaciadas na rota de transactions

--- TypeScript/projeto/src/routes/transactions.route.ts
+++ TypeScript/projeto/src/routes/transactions.route.ts
@@ -2,12 +2,14 @@ import { Router } from 'express'
 
 import { ParamsType, validator } from '../middlewares/validator.middleware'
 import { createTransactionSchema } from '../dtos/transactions.dto'
+import { TransactionsController } from '../controllers/transactions.controller'
+import { TransactionsFactory } from '../factories/transactions.factory'
 
 export const transactionsRoutes = Router() // Permite a criação de rotas
 
-/* const controller = new CategoriesController(  // Instanciando a classe
-    CategoriesFactory.getServiceInstance()
-) */
+const controller = new TransactionsController(  // Instanciando a classe
+    TransactionsFactory.getServiceInstance()
+)
 
 transactionsRoutes.post(
     '/', 
