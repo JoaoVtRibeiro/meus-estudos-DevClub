2fd6bd8410dc5532010682f2e4c97c2e6e8fcc77
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-04-14 05:49:11 -0300

    Projeto Fullstack - Correção dos erros da sessão de adm, match e prop types

--- Projeto-Fullstack/front-end/src/containers/Admin/index.js
+++ Projeto-Fullstack/front-end/src/containers/Admin/index.js
@@ -1,25 +1,33 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import { SideMenuAdmin } from '../../components/SideMenuAdmin'
-import Orders from './Orders'
+import paths from '../../constants/path'
+import EditProduct from './EditProduct'
 import ListProducts from './ListProducts'
 import NewProduct from './NewProduct'
-import EditProduct from './EditProduct'
-import paths from '../../constants/path'
+import Orders from './Orders'
+
 import { Container, ContainerItems } from './styles.js'
 
-export function Admin({ math: { path } }) { // props dos componentes em components/SideMenuAdmin (caso aja duvida: console.log(props)) *obs fim da página
+export function Admin({ match: { path } }) { // props dos componentes em components/SideMenuAdmin (caso aja duvida: console.log(props)) *obs fim da página
     return (
         <Container>
             <SideMenuAdmin path={path} />
             <ContainerItems>
-                {path === paths.OrderAdm && <Orders /> } {/* Se path for igual a url então renderizar componente */}
-                {path === paths.ListProductsAdm && <ListProducts /> }
-                {path === paths.NewProductAdm && <NewProduct /> }
-                {path === paths.EditProductsAdm && <EditProduct /> }   
+                {path === paths.OrderAdm && <Orders />} {/* Se path for igual a url então renderizar componente */}
+                {path === paths.ListProductsAdm && <ListProducts />}
+                {path === paths.NewProductAdm && <NewProduct />}
+                {path === paths.EditProductsAdm && <EditProduct />}
             </ContainerItems>
         </Container>
     )
+}
+
+Admin.propTypes = {
+    match: PropTypes.shape({
+        path: PropTypes.string
+    })
 }
 
 /* 
