a2f461e95f50dec2e8732a279edce62b398872f6
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-04-26 10:07:00 -0300

    React Bônus - Anotações sobre a Node_Modules e descrições do arquivos inicias

--- React-Bonus/projeto/src/main.jsx
+++ React-Bonus/projeto/src/main.jsx
@@ -1,10 +1,19 @@
+/* Nesse arquivo serão importados os componentes para renderização */
+
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client' // Permite manipular a DOM (arvore de elementos html)
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+/* 
+  ReactDOM "cria uma raiz" (indo ao html.pegando um id('chamado root')).dentro desse elemento ele renderiza(
+    <>
+      <Renderizando o App />
+    <>
+  )
+*/
