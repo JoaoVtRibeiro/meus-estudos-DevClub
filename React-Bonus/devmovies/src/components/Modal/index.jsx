aa5d54058a92d464d67b3b4946785634925be91e
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-05-03 14:09:51 -0300

    React Bônus - Ícones da aba do navegador

--- React-Bonus/devmovies/src/components/Modal/index.jsx
+++ React-Bonus/devmovies/src/components/Modal/index.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 
-import api from '../../services/api'
 import { Container, Background } from './styles'
 import { getMovieTrailer } from '../../services/getData'
 
