a2bdaa12b69dd31968929a84566cf5f1b0d56b23
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-06-15 12:10:16 -0300

    Javascript - Métodos de arrays (some)

--- Javascript/Map Reduce e Filter/Metodos de arrays/metodos.js
+++ Javascript/Map Reduce e Filter/Metodos de arrays/metodos.js
@@ -6,7 +6,7 @@
         sort      -> Organiza o array
         delete    -> Deleta o item de uma posição do Array (logo a posição terá "vazio" como valor)
         every     -> Esse método testa se todos os elementos do array passam em uma condição.
-        some      -> O .some() faz algo parecido com o .every(). A diferença é que o .every()
+        some      -> O .some() faz algo parecido com o .every(). A diferença é que o .every() retorna true se todos passarem, some precisa que apenas um passe
         fill      -> Preenche posições do array com desejarmos
         find      -> Usamos esse método quando queremos encontrar algum elemento dentro no Array
         findIndex   -> Faz o mesmo que o .find(), mas retorna o índice do elemento encontrado ao
@@ -23,6 +23,12 @@
 
 const numbers = [1, 2, 3]
 const fruits = ['limão', 'amora', 'goiaba']
+const users = [
+    { name: 'Nico', credit: 600},
+    { name: 'Maria', credit: 900},
+    { name: 'Joana', credit: 300},
+    { name: 'Vanessa', credit: 200}
+]
 
 // push, adiciona um item ao array
 numbers.push(4)
@@ -39,15 +45,8 @@ delete fruits[1]
 console.log(fruits)
 
 // every, verifica se TODOS os itens passam por um validação posta, resultando em true ou false
-
-const users = [
-    { name: 'Nico', credit: 600},
-    { name: 'Maria', credit: 900},
-    { name: 'Joana', credit: 300},
-    { name: 'Vanessa', credit: 200}
-]
-
 console.log(users.every(user => user.credit > 100))
 
+// some, ao contrario do every, some resulta true se qualquer um dos itens for de acordo a validação
 
-
+console.log(users.some(user => user.credit > 800))
