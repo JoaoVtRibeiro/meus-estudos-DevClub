64d0e27e4f230932fb3172f88f10d4aae5745378
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-06-18 08:49:03 -0300

    Javascript - Metódo de Arrays, concat

--- Javascript/Map Reduce e Filter/Metodos de arrays/metodos.js
+++ Javascript/Map Reduce e Filter/Metodos de arrays/metodos.js
@@ -10,7 +10,7 @@
         fill      -> Preenche posições do array como desejarmos, de acordo com os 3 parâmetros
         find      -> Usamos esse método quando queremos encontrar algum elemento dentro no Array
         findIndex   -> Faz o mesmo que o .find(), mas retorna o índice (posição) do elemento encontrado
-        concat()    ->
+        concat()    -> Usado para juntar dois arrays. Ele não altera os arrays originais, mas cria um novo com a soma dos dois
         
         includes  -> O método includes() determina se um array co
         join      -> O método join() junta todos os elementos de um
@@ -23,12 +23,6 @@
 
 const numbers = [1, 2, 3]
 const fruits = ['limão', 'amora', 'goiaba']
-const users = [
-    { name: 'Nico', credit: 600},
-    { name: 'Maria', credit: 900},
-    { name: 'Joana', credit: 300},
-    { name: 'Vanessa', credit: 200}
-]
 
 // push, adiciona um item ao array
 numbers.push(4)
@@ -45,6 +39,13 @@ delete fruits[1]
 console.log(fruits)
 
 // every, verifica se TODOS os itens passam por um validação posta, resultando em true ou false
+const users = [
+    { name: 'Nico', credit: 600},
+    { name: 'Maria', credit: 900},
+    { name: 'Joana', credit: 300},
+    { name: 'Vanessa', credit: 200}
+]
+
 console.log(users.every(user => user.credit > 100))
 
 // some, ao contrario do every, some resulta true se qualquer um dos itens for de acordo a validação
@@ -75,3 +76,11 @@ console.log(`find: ${findAUser}`)
 
 const findIndexForAUser = users.findIndex(user => user.name === 'Maria')
 console.log(`findIndex: ${findIndexForAUser}`)
+
+// concat, faz um novo array juntando dois arrays diferentes
+
+const array1 = ['a', 'b', 'c']
+const array2 = ['d', 'e', 'f']
+
+const array3 = array1.concat(array2)
+console.log(array3)
