c7b7dedcd42d66ee2f107816fb483be3fee1dbd8
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-06-27 12:04:59 -0300

    Javacript - Lógica do forEach remodulada e Botão de disconto (logica map)

--- Javascript/Map Reduce e Filter/projeto - arrays/scripts.js
+++ Javascript/Map Reduce e Filter/projeto - arrays/scripts.js
@@ -1,28 +1,49 @@
 import { menuOptions } from './products.js'
 
-const showAllButton = document.getElementById("show-all")
-const discountButton = document.getElementById("discount")
-const someAllButton = document.getElementById("some-all")
-const veganButton = document.getElementById("vegan")
-const ul = document.querySelector("ul")
+const showAllButton = document.getElementById('show-all')
+const discountButton = document.getElementById('discount')
+const someAllButton = document.getElementById('some-all')
+const veganButton = document.getElementById('vegan')
+const ul = document.querySelector('ul')
 
-showAllButton.addEventListener("click", function () {
+// Mostrar todos os produtos
+showAllButton.addEventListener('click', function () {
     ul.innerHTML = ''
+    let li = '' // a li precisa desse '' para não iniciar undefined, estando undefined o forEach irá criar uma li vazia no primeiro valor
+    
+    menuOptions.forEach(product => {
+        // += para adicionar um novo, e não sobre escrever (myli = myli + `...`)
+        li += `
+            <li>
+                <img src="${product.src}">
+                <p>${product.name}</p>
+                <p class="price">${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.price)}</p>
+            </li>
+        `
 
-    menuOptions.forEach((product) => {
-        const li = document.createElement("li")
-        const img = document.createElement("img")
-        const title = document.createElement("p")
-        const price = document.createElement("p")
+        ul.innerHTML = li
+    })
+})
 
-        img.src = product.src
-        title.innerHTML = product.name
-        price.innerHTML = `R$ ${product.price},00`
+// Mostrar todos os produtos com 10% de desconto
+discountButton.addEventListener('click', function () {
+    ul.innerHTML = ''
+    let li = ''
+    let priceDiscount
 
-        li.appendChild(img)
-        li.appendChild(title)
-        li.appendChild(price)
-        ul.appendChild(li)
+
+    menuOptions.map(product => {
+        priceDiscount = product.price - (product.price/10)
+
+        li += `
+            <li>
+                <img src="${product.src}">
+                <p>${product.name}</p>
+                <p class="price">${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(priceDiscount)}</p>
+            </li>
+        `
+
+        ul.innerHTML = li
     })
 })
 
