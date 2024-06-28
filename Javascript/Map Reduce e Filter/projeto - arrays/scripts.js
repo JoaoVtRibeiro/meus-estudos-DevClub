b6c492f216b1aa70f1ef748671a3419dbc37a93e
jvribeirodev <joaoribeiro.cpu@gmail.com>
2024-06-28 12:08:58 -0300

    Javascript - Função para mostrar as arrays em geral, remodulagem na lógica de desconto e Botão de soma geral do itens

--- Javascript/Map Reduce e Filter/projeto - arrays/scripts.js
+++ Javascript/Map Reduce e Filter/projeto - arrays/scripts.js
@@ -2,16 +2,16 @@ import { menuOptions } from './products.js'
 
 const showAllButton = document.getElementById('show-all')
 const discountButton = document.getElementById('discount')
-const someAllButton = document.getElementById('some-all')
+const someAllButton = document.getElementById('sum-all')
 const veganButton = document.getElementById('vegan')
 const ul = document.querySelector('ul')
 
-// Mostrar todos os produtos
-showAllButton.addEventListener('click', function () {
-    ul.innerHTML = ''
+// Função "principal" que recebe a array modificada e mostra na tela
+function showAll(productArray) {
+    ul.innerHTML = '' // "limpa" a tela do array anterior para não ficar 2 ou mais juntos na tela
     let li = '' // a li precisa desse '' para não iniciar undefined, estando undefined o forEach irá criar uma li vazia no primeiro valor
-    
-    menuOptions.forEach(product => {
+
+    productArray.forEach(product => {
         // += para adicionar um novo, e não sobre escrever (myli = myli + `...`)
         li += `
             <li>
@@ -23,27 +23,33 @@ showAllButton.addEventListener('click', function () {
 
         ul.innerHTML = li
     })
+}
+
+// Array dos produtos sem nenhuma modificação
+showAllButton.addEventListener('click', () => {
+    showAll(menuOptions)
 })
 
-// Mostrar todos os produtos com 10% de desconto
-discountButton.addEventListener('click', function () {
+// Array com 10% de desconto
+discountButton.addEventListener('click', () => {
     ul.innerHTML = ''
     let li = ''
-    let priceDiscount
 
+    const menuWithDiscount = menuOptions.map(product => ({
+        ...product,
+        price: product.price - (product.price / 10),
+    }))
 
-    menuOptions.map(product => {
-        priceDiscount = product.price - (product.price/10)
+    showAll(menuWithDiscount)
+})
 
-        li += `
-            <li>
-                <img src="${product.src}">
-                <p>${product.name}</p>
-                <p class="price">${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(priceDiscount)}</p>
-            </li>
-        `
+someAllButton.addEventListener('click', () => {
+    const finalValue = menuOptions.reduce((acc, product) => acc + product.price, 0)
 
-        ul.innerHTML = li
-    })
+    ul.innerHTML = `
+        <li>
+            <p>A soma de todos os itens do menu é: ${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(finalValue)}</p>
+        </li>
+    `
 })
 
